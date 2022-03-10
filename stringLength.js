99175d90b46f489ea14eb659c9e5bc3154866072
beyk <beykmail@gmail.com>
2022-03-10 22:49:05 +0330

    Add condition 0<string<10 to our logic and complate test for it

--- stringLength.js
+++ stringLength.js
@@ -1,7 +1,10 @@
-function stringLength(str)
+function stringLength(string)
 {
-  
-  return  [...str].length;
-  
+  if(string.length>0 && string.length<10){
+    return  string.length;
+    }
+  else
+    throw new Error('the string does not meet the reqs')
 }
 module.exports = stringLength;
+//console.log(stringLength("dgi"))
--- stringLength.test.js
+++ stringLength.test.js
@@ -1,7 +1,15 @@
-const { expect, test } = require("@jest/globals");
+
 const stringLength = require("./stringLength");
-const strLength = require("./stringLength");
+
 const testStr="hello";
 test("The length of Hello is 5",() =>{
-    return expect(stringLength(testStr)).toBe(5);
+     expect(stringLength(testStr)).toBe(5);
+})
+
+ test("The string is too short",()=>{
+     expect(() => {stringLength('');}).toThrowError(new Error('the string does not meet the reqs'));;
+ })
+
+ test("The string is too long",()=>{
+    expect(() => {stringLength('12345678901');}).toThrowError(new Error('the string does not meet the reqs'));;
 })
