0ada53b6951bb0e97affbad4a9e1323df464f13f
beyk <beykmail@gmail.com>
2022-03-11 11:16:34 +0330

    Add reverse function and testfunction for it

--- stringLength.js
+++ stringLength.js
@@ -1,10 +1,17 @@
-function stringLength(string)
-{
-  if(string.length>0 && string.length<10){
-    return  string.length;
-    }
-  else
+function stringLength(string) {
+  if(string.length < 1 || string.length > 10) {
     throw new Error('the string does not meet the reqs')
+  } else {
+    return string.length;
+  }
 }
-module.exports = stringLength;
-//console.log(stringLength("dgi"))
+
+function reverseString(string) {
+  return string.split('').reverse().join('');
+}
+module.exports = {
+  stringLength,
+  reverseString
+};
+// console.log(stringLength("036547896"))
+// console.log(reversString("01236547896"))
--- stringLength.test.js
+++ stringLength.test.js
@@ -1,15 +1,20 @@
+const { stringLength, reverseString } = require('./stringLength.js');
 
-const stringLength = require("./stringLength");
 
-const testStr="hello";
 test("The length of Hello is 5",() =>{
-     expect(stringLength(testStr)).toBe(5);
+     expect(stringLength("Hello")).toBe(5);
 })
 
  test("The string is too short",()=>{
-     expect(() => {stringLength('');}).toThrowError(new Error('the string does not meet the reqs'));;
+     expect(() => {stringLength('');}).toThrowError(new Error('the string does not meet the reqs'));
  })
 
  test("The string is too long",()=>{
-    expect(() => {stringLength('12345678901');}).toThrowError(new Error('the string does not meet the reqs'));;
+    expect(() => {stringLength('this text is too long');}).toThrowError(new Error('the string does not meet the reqs'));
 })
+test('"messi" reversed should be "issem"', () => {
+    expect(reverseString('messi')).toMatch('issem');
+  });
+// // test("hello must be reversed to olleh",() =>{
+// //     expect(reversString("hello")).toMatch("olleh");
+// // })
