1091bb652920b16e0aaa8a82294bb6bb2bd6d4cb
beyk <beykmail@gmail.com>
2022-03-11 11:38:56 +0330

    Complate calculator logic and test funtions

--- calculator.test.js
+++ calculator.test.js
@@ -1,14 +1,17 @@
+const {add, subtract, multiply, divide } = require("./calculator");
+
+
 describe("calculator testing block",() =>{
     test('sum of 2+3 is 5',() =>{
         expect(add(2,3)).toBe(5);
     })
     test('subtract of 5-2 is 3',() =>{
-        expect(add(5,2)).toBe(3);
+        expect(subtract(5,2)).toBe(3);
     })
-    test('sum of 2*3 is 6',() =>{
-        expect(add(2,3)).toBe(6);
+    test('multiply of 2*3 is 6',() =>{
+        expect(multiply(2,3)).toBe(6);
     })
-    test('sum of 6/2 is 3',() =>{
-        expect(add(6,2)).toBe(3);
+    test('divide of 6/2 is 3',() =>{
+        expect(divide(6,2)).toBe(3);
     })
 })
