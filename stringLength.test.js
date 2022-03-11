5bcafd7707504e1d20dae7439cb2c49d64b30bd3
beyk <beykmail@gmail.com>
2022-03-11 11:43:43 +0330

    Add capitalize(string) test function

--- stringLength.test.js
+++ stringLength.test.js
@@ -12,9 +12,12 @@ test("The length of Hello is 5",() =>{
  test("The string is too long",()=>{
     expect(() => {stringLength('this text is too long');}).toThrowError(new Error('the string does not meet the reqs'));
 })
-test('"messi" reversed should be "issem"', () => {
-    expect(reverseString('messi')).toMatch('issem');
+test('"hello" reversed should be "olleh"', () => {
+    expect(reverseString('hello')).toMatch('olleh');
   });
+test("capitalize the first letter of string:hello will change to Hello",() => {
+  expect(capitalize("hello")).toMatch("Hello");
+})
 // // test("hello must be reversed to olleh",() =>{
 // //     expect(reversString("hello")).toMatch("olleh");
 // // })
